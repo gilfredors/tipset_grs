1c722fb4f94b96166bbe8ed751817e25b5853615
Gilfredo Remon <remong89@gmail.com>
2020-08-29 21:37:48 +0200

    Updated runner program to set a minimal starting point

--- runner.py
+++ runner.py
@@ -1,8 +1,13 @@
 import os
+from online_data.data_fetcher import DataFetcher
 
 
 def main():
     print("Welcome to GRS's custom football tips predictor")
+    log_dir = os.path.join(os.path.dirname(__file__), 'online_data', 'log')
+    f = DataFetcher(log_dir)
+    data = f.get('countries')
+    print('EXIT')
 
 
 if '__main__' == __name__:
